6b75dc6d9005a6afb5590f578ca9b25ad3f3a29f
Mohamed Eslam <mohamedeslamfekry307@gmail.com>
2024-04-25 12:46:07 -0400

    Update hmm.h
    added some comments

--- hmm.h
+++ hmm.h
@@ -7,10 +7,9 @@
 
 #define METADATA_SIZE 8 // Size of metadata stored with each allocated memory block
 #define SBRK_ALLOC_SIZE (33*4096) // Size of memory to request from OS using sbrk
-#define MIN_FREE_SBRK (15*4096) // Minimum size of free memory to release using sbrk
+#define MIN_FREE_SBRK (28*4096) // Minimum size of free memory to release using sbrk
 
-void *realloc(void *ptr, size_t size);
-void *calloc(size_t nmemb, size_t size);
+
 /**
  * @brief Allocates memory on the heap
  *
@@ -39,5 +38,41 @@ void *malloc(size_t size);
  * @return Nothing
  */
 void free(void *ptr);
+
+/**
+ * Resizes a previously allocated memory block.
+ *
+ * This function attempts to resize the memory block pointed to by `ptr`. The new size is specified by `size` bytes.
+ * It offers a way to modify the amount of memory allocated for a given block without having to manually free and reallocate.
+ *
+ * There are three main cases handled by `realloc`:
+ *  - If `ptr` is NULL, it behaves exactly like `malloc(size)`.
+ *  - If `size` is zero, it frees the memory pointed to by `ptr` and returns NULL.
+ *  - If `size` is larger than the current allocation, it allocates a new block of memory, copies the data from the old block,
+ *    and frees the old block. Otherwise, it returns the original pointer (`ptr`).
+ *
+ * @param ptr    A pointer to the previously allocated memory block, or NULL if requesting a new allocation.
+ * @param size   The new size of the memory block in bytes. If `size` is zero, the function frees the memory pointed to by `ptr`.
+ *
+ * @return  A pointer to the reallocated memory block on success, NULL on failure.
+ *          Failure can occur if there is not enough memory available for the requested size, or if `ptr` is invalid.
+ */
+void *realloc(void *ptr, size_t size);
+/**
+ * Allocates memory and initializes it to zero.
+ *
+ * This function allocates a contiguous block of memory of size `nmemb * size` bytes.
+ * If successful, it initializes all bytes in the allocated block to zero. This ensures that
+ * any uninitialized data is set to a known value, which can help prevent security vulnerabilities
+ * and unexpected behavior.
+ *
+ * @param nmemb  The number of elements to allocate. If `nmemb` is zero, the function returns NULL.
+ * @param size   The size of each element in bytes. If `size` is zero, the function returns NULL.
+ *
+ * @return  A pointer to the allocated memory block on success, NULL on failure.
+ *          Failure can occur if the product of `nmemb` and `size` overflows `size_t`, or if there
+ *          is not enough memory available to satisfy the request.
+ */
+void *calloc(size_t nmemb, size_t size);
 
 #endif  // HMM_H
